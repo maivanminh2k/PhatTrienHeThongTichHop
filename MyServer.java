7478647bbf7d5119d10e0c762bc40eae879e30cf
maivanminh2k <72431215+maivanminh2k@users.noreply.github.com>
2020-10-13 14:50:58 +0700

    Lab3/Bai2

--- MyServer.java
+++ MyServer.java
@@ -1,31 +1,37 @@
-package iPAddress;
+package Bai2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
-
 public class MyServer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			ServerSocket server = new ServerSocket(9540);
+			ServerSocket server = new ServerSocket(1100);
 			System.out.println("Server da duoc tao");
 			Socket client = server.accept();
 			System.out.println("Client da ket noi den server");
 			Scanner inFromClient = new Scanner(client.getInputStream());
 			PrintStream outToClient = new PrintStream(client.getOutputStream());
-			outToClient.println("Xin chao, ban ten gi?");
-			String ten = inFromClient.nextLine();
-			System.out.println("clien: " + ten);
-			outToClient.println("Rat vui duoc tro chuyen voi " + ten);
+			outToClient.println("nhap ten sach");
+			String name = inFromClient.nextLine();
+			System.out.println("client: " +name);
+			
+			 BufferedReader br = null;
+		            br = new BufferedReader(new FileReader("C:\\Users\\student18\\text.txt"));       
+		            String textInALine = br.readLine();
+			if(textInALine.contains(name))
+			outToClient.println("co sach ");
+			else outToClient.println("khong co sach ");
 			} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			}
 
 	}
-
 }
